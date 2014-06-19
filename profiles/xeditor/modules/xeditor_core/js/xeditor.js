1bae3ee874150ef50978d49d4f2e6a0425737b7e
Gerald Z. Villorente <geraldvillorente@gmail.com>
2014-06-19 22:22:41 +0800

    xeditor-barley js

--- profiles/xeditor/modules/xeditor_core/js/xeditor.js
+++ profiles/xeditor/modules/xeditor_core/js/xeditor.js
@@ -2,3 +2,12 @@
  * @file
  * xEditor_core Barley. Provides Barley-like functionality.
  */ 
+(function ($) {
+
+  Drupal.behaviors.xeditor = {
+    attach: function (context, settings) {
+      // Do something
+      console.log(settings);
+    }
+  };
+})(jQuery);
