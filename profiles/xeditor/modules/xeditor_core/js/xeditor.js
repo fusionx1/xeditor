f3f298b528cc6213ae189f5ddf89a0087edee7bc
André Drougge <andre.drougge@gmail.com>
2014-07-25 20:44:04 +0300

    First steps towards ajaxifying cleditor saves. Also split up some js functionality.

--- profiles/xeditor/modules/xeditor_core/js/xeditor.js
+++ profiles/xeditor/modules/xeditor_core/js/xeditor.js
@@ -1,120 +1,172 @@
 /**
  * @file
- * xEditor_core Barley. Provides Barley-like functionality.
+ * xEditor Barley. Provides Barley-like functionality.
  */
 (function ($) {
 
-  Drupal.behaviors.xeditorCore = {
-    // This behavior function is called when new element is being added.
-    attach: function (context, settings) {
-      // Do your js stuffs you want to do after your AJAX call.
-      // ******************************************************
-      // Load editable fields when tapping content.
-      // See - http://api.jquery.com/trigger/.
-      $(".front .node .field-name-body, .node .field-name-body, .node h2 a, .page-node h1.title").click(function() {
-        $(".quick-edit a").trigger("click");
-        setTimeout(function() {
-          if ($(".page-node h1.title > div").hasClass("edit-editable")) {
-            $(".page-node h1.title > div").trigger("click").focus();
-          }
-        }, 50);
+  Drupal.xeditor = {
+
+    init: function() {
+      this.applyCKEEvents();
+    },
+
+    /**
+     * getSelection()
+     * - Returns the current selection from window
+     * - Takes no arguments
+     */
+    getSelection: function() {
+      var text = "";
+      if (window.getSelection) {
+        text = window.getSelection().toString();
+      }
+      else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+      }
+
+      return text;
+    },
+
+    /**
+     * makeSelection(CKeditor, string)
+     * - Applies selection to editor
+     * TODO: finish this function, this is just a copy/paste
+     */
+    makeSelection: function(editor, selection) {
+      // Grabs the current selection so that we can
+      // reapply it to the editable content in CKE
+      var sel = editor.getSelection();
+
+      // Change the selection to the current element.
+      var element = sel.getStartElement();
+      sel.selectElement(element);
+
+      // Move the range to the we selected earlier.
+      var findString = Drupal.xeditor.getSelection();
+      var ranges = editor.getSelection().getRanges();
+      var startIndex = element.getHtml().indexOf(findString);
+      if (startIndex != -1 && (typeof ranges[0] != 'undefined')) {
+        ranges[0].setStart(element.getFirst(), startIndex);
+        ranges[0].setEnd(element.getFirst(), startIndex + findString.length);
+        sel.selectRanges([ranges[0]]);
+      }
+    },
+
+    loadScript: function(url, callback) {
+      // Adding the script tag to the head as suggested before
+      var head = document.getElementsByTagName('head')[0];
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = url;
+
+      // Then bind the event to the callback function.
+      // There are several events for cross browser compatibility.
+      script.onreadystatechange = callback;
+      script.onload = callback;
+
+      // Fire the loading
+      head.appendChild(script);
+    },
+
+    setTitleSettings: function(editor) {
+      console.warn('Setting title settings for CKE');
+
+      // Remove unnecessary plugins to make the editor simpler.
+      var unwantedPlugins = [
+        'colorbutton',
+        'find',
+        'flash',
+        'font',
+        'forms',
+        'iframe',
+        'image',
+        'newpage',
+        'removeformat',
+        'smiley',
+        'specialchar',
+        'stylescombo',
+        'templates'
+      ];
+
+      editor.config.removePlugins = unwantedPlugins.join(',');
+
+      // Rearrange the layout of the toolbar.
+      editor.config.toolbarGroups = [
+        { name: 'editing',    groups: [ 'basicstyles', 'links' ] },
+        { name: 'undo' },
+        { name: 'clipboard',  groups: [ 'selection', 'clipboard' ] },
+        { name: 'about' }
+      ];
+    },
+
+    shouldBeHandledAsTitle: function($el) {
+      var titles = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+      return ($el.is(titles) || $el.hasAncestor(titles));
+    },
+
+    stripNid: function(nid) {
+      return parseInt(nid.match(/\d{1,}$/g));
+    },
+
+    // TODO: Finish this function
+    saveContent: function(nid, html) {
+      var ajx = $.ajax({
+        url: 'profiles/xeditor/modules/xeditor_core/xeditor_core_ajax.php',
+        type: 'POST',
+        dataType: 'JSON',
+        data: { nid: nid, content: html }
       });
 
-      // Load ckeditor.
-      var element = $(".node .field-name-body .field-item");
-      $(element).mouseup(function(){
-        var selection = Drupal.xeditor.getSelection();
-        if (selection.length != 0) {
-          setTimeout(function() {
-            if ($(".node .field-name-body, .node .field-name-body .field-item").hasClass("edit-editable")) {
-              $(".node .field-name-body.edit-editable").trigger("click");
-              $(".node .field-name-body .edit-editable").trigger("click");
+      ajx.fail(function(xhr, status, er) {
+        console.info('Saving node resulted in this: ');
+        console.error(er);
+      });
+
+      ajx.always(function(xhr, status, er) {
+        console.info('Ajax done..');
+        console.info(status);
+        console.log(xhr);
+      });
+    },
+
+    applyCKEEvents: function() {
+      var that = this;
+
+      CKEDITOR.plugins.registered['save'] = {
+       init : function(editor) {
+        var command = editor.addCommand('save', {
+            modes : { wysiwyg:1, source:1 },
+            exec : function( editor ) {
+              var html = editor.getData(),
+               nidAttr = $(editor.element.$).closest('.node').attr('id'),
+                  nid  = that.stripNid(nidAttr);
+              that.saveContent(nid, html);
             }
-          }, 90);
+          }
+        );
+
+        editor.ui.addButton( 'Save', { label : 'Save changes', command : 'save', toolbar: 'basicstyles,1' });
+       }
+      };
+
+      CKEDITOR.on('instanceCreated', function(e) {
+        var editor  = e.editor,
+            element = editor.element;
+
+        if (Drupal.xeditor.shouldBeHandledAsTitle($(element.$))) {
+          editor.on('configLoaded', function() {
+            that.setTitleSettings(editor);
+          });
         }
       });
 
-      // This is to activate Ckeditor native inline-edit.
-      // jQuery(".node .field-name-body").attr("contenteditable", "true");
-    },
-
-    // This behavior function is called when elements are removed.
-    detach: function (context, settings) {
-      // Do your js stuffs you want to do after your AJAX call.
+      CKEDITOR.on("instanceReady", function(e) {
+        // Simple shout-out about what instances exists atm.
+        for(var instanceName in CKEDITOR.instances) {
+          // console.log(CKEDITOR.instances[instanceName]);
+        }
+      });
     }
-  };
-
-  /**
-   * Create a new object called 'xeditor'.
-   */
-  Drupal.xeditor = new Object;
-
-  /**
-   * Get text selection.
-   */
-  Drupal.xeditor.getSelection = function() {
-    var text = "";
-    if (window.getSelection) {
-      text = window.getSelection().toString();
-    }
-    else if (document.selection && document.selection.type != "Control") {
-      text = document.selection.createRange().text;
-    }
-
-    return text;
-  };
-
-  /**
-   * Pre-Load Ckeditor script via AJAX.
-   */
-  Drupal.xeditor.loadScript = function(url, callback) {
-    // Adding the script tag to the head as suggested before
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
-    script.onreadystatechange = callback;
-    script.onload = callback;
-
-    // Fire the loading
-    head.appendChild(script);
-  };
-
-  /**
-   *  Pass user selection to Ckeditor instance.
-   */
-  CKEDITOR.on("instanceReady", function(event) {
-    var editor = event.editor;
-    var sel = editor.getSelection();
-
-    // Change the selection to the current element.
-    var element = sel.getStartElement();
-    sel.selectElement(element);
-
-    // Move the range to the we selected earlier.
-    var findString = Drupal.xeditor.getSelection();
-    var ranges = editor.getSelection().getRanges();
-    var startIndex = element.getHtml().indexOf(findString);
-    if (startIndex != -1 && (typeof ranges[0] != 'undefined')) {
-      ranges[0].setStart(element.getFirst(), startIndex);
-      ranges[0].setEnd(element.getFirst(), startIndex + findString.length);
-      sel.selectRanges([ranges[0]]);
-    }
-
-  });
-
-  /**
-   * Get Ckeditor instance.
-   */
-  Drupal.xeditor.getInstances = function() {
-    CKEDITOR.on("instanceReady", function(event) {
-      for(var instanceName in CKEDITOR.instances) {
-        console.log(CKEDITOR.instances[instanceName]);
-      }
-    });
   };
 
 })(jQuery);
